a7e4c44f49306373fc2dca63bb01b35e00a83928
umar shafiq <umarshafiq@umars-iMac.local>
2021-10-20 03:14:02 +0500

    new push

--- server.js
+++ server.js
@@ -32,15 +32,14 @@ const helmet = require("helmet");
 //   next();
 // });
 app.use((req, res, next) => {
-  //allow access from every, elminate CORS
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.removeHeader("x-powered-by");
-  //set the allowed HTTP methods to be requested
-  res.setHeader("Access-Control-Allow-Methods", "POST");
-  //headers clients can use in their requests
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  //allow request to continue and be handled by routes
-  next();
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
 });
 //wearing a helmet
 
